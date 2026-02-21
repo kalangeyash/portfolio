54b72bb4d4e59ca4b73831a5ea659e6aad2514f6
Yash Kalange <yashkalange127@gmail.com>
2026-02-21 21:34:18 +0530

    images.domains fixed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['api.microlink.io'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.microlink.io",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
